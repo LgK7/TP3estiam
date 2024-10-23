b0a13e8be05213559b10e47f7dd0989fd4257786
LgK7 <119427128+LgK7@users.noreply.github.com>
2024-10-23 14:16:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-const http = require(http'):
-
-const server = http.createServer(req, res) => 1
-
-res.statusCode = 200;
-
-res setHeader ('Content-Type', 'text/plain*):
-
-res. end ('Hello World\n*);
-
-7):
-
+const http = require('http');
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+ 
 const port = process.env.PORT || 3000;
-
-server.listen (port, 0=>*
-
-console.log( Server running at http://localhost:S(port)/);
-
-);
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
